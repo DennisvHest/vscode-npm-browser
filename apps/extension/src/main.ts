d84263bf87c690b816fd9e0429cbb2844a2769f1
Dennis van Hest <dennisvhest@hotmail.nl>
2019-12-31 18:41:56 +0100

    Can open browser by clicking sidebar button

--- apps/extension/src/main.ts
+++ apps/extension/src/main.ts
@@ -3,13 +3,36 @@ import { BrowserWebView } from './app/BrowserWebView';
 import { NPMTerminal } from './app/NPMTerminal';
 import { CommandTypes, ToastLevels, ValueCommand, VSCodeToastCommand } from '../../../libs/shared/src';
 
+class DependencyTreeDataProvider implements vscode.TreeDataProvider<Object> {
+
+	readonly onDidChangeTreeData: vscode.Event<Object> = new vscode.EventEmitter<Object>().event;
+
+	getTreeItem(element: Object): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+
+	getChildren(element?: Object): vscode.ProviderResult<Object[]> {
+		return;
+	}
+
+	getParent?(element: Object): vscode.ProviderResult<Object> {
+		return;
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	const npmTerminal = new NPMTerminal();
 
 	context.workspaceState.update('selectedPackageJson', null);
 
-	const disposable = vscode.commands.registerCommand('npmBrowser.open', async () => {
+	const treeView = vscode.window.createTreeView("dependencies", {
+		treeDataProvider: new DependencyTreeDataProvider()
+	});
+
+	treeView.onDidChangeVisibility(async (event) => {
+		if (!event.visible)
+			return;
 
 		context.workspaceState.update('packageJsons', await npmTerminal.findPackageJsons());
 
@@ -29,13 +52,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 		npmTerminal.onPackageJsonChange = changedPackageJson => {
 			context.workspaceState.update('selectedPackageJson', changedPackageJson);
-			
+
 			const command: ValueCommand = { type: CommandTypes.packageJsonUpdated, value: changedPackageJson };
 			browser.sendCommand(command);
 		}
 	});
-
-	context.subscriptions.push(disposable);
 
 	function onValueCommand(command: ValueCommand) {
 		npmTerminal.packageJson = command.value;
